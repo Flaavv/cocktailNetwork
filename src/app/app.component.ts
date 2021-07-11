48020feae97b9e4185941c44bee1d3438700bb3c
theMaxiFlav <laffont.flavien@gmail.com>
2021-07-11 02:17:08 +0200

    v

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,11 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 
 export class AppComponent implements OnInit {
+
   title = 'CocktailNetwork';
-
-
-
-  ngOnInit(){
-   
+  ngOnInit(): void {
+    console.log('Im workingg')
   }
+
+
+
 }
